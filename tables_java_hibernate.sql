a05d05137441133390cfb0f6a923495e65bc986e
gagochorenmartin@gmail.com <chgnitram@gmail.com>
2024-06-20 21:12:33 +0200

    creada tabla carrito

--- tables_java_hibernate.sql
+++ tables_java_hibernate.sql
@@ -1,4 +1,4 @@
-DROP TABLE  IF EXISTS usuarios;
+-- DROP TABLE  IF EXISTS usuarios;
 
 CREATE TABLE IF NOT EXISTS usuarios(
 identificador INT(20) AUTO_INCREMENT PRIMARY KEY,
@@ -7,5 +7,11 @@ user_password varchar(200) NOT NULL,
 user_email varchar(50) NOT NULL,
 user_date_creation datetime NOT NULL,
 user_last_connection datetime NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS usuarios_carrito(
+identificador INT, 
+
+CONSTRAINT fk_carrito FOREIGN KEY (identificador) 
+REFERENCES usuarios(identificador)
 );
